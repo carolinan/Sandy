268c04e0c5b4c9b69de0e8ff868c41d65497d32d
Sandilya Kafle <kafleg@users.noreply.github.com>
2021-06-18 12:07:03 +0545

    Update index.php
    Added a new sentence on index.php file with new PR for it.

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 I am blank. Edit me.
 
 Edited this file after pushing README.md in update/readme branch. And I made a new branch for this change. But the second edit on README.md file was pushed and didn't make PR again.
+
+Edited this file again. I think i got the point. :) Pushing on same branch for index.php file.
